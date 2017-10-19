08913df12bd8c959b597f75e19920db100dea791
demonxian3 <demonxian3@qq.com>
2017-10-19 17:13:48 +0800

    MulitBFS have some problem

--- graph/breathFirst/AdjListDG_BFS.c
+++ graph/breathFirst/AdjListDG_BFS.c
@@ -3,6 +3,8 @@
 #define MAXNUM 100
 #define True 1
 #define False 0
+#define OK 1
+#define ERROR -2
 
 typedef char VertexType;
 
@@ -16,13 +18,99 @@ typedef struct VNode{
   struct EdgeNode *firstedge;
 }VNode,AdjList[MAXNUM];
 
-typedef struct {
+typedef struct{
   AdjList vexs;
   int vexnum,acrnum;
 }VNGraph;
 
+/***********************************/
+/*		Queue		   */
+/***********************************/
+typedef int Status;
+typedef int ElemType;
+typedef struct Qnode{
+  ElemType data;
+  struct Qnode *next;
+}Qnode,*Qlink;
+
+typedef struct{
+  Qlink front;
+  Qlink rear;
+}Queue;
+
+Status initQueue(Queue *Q){
+  Q->front = Q->rear = (Qlink)malloc(sizeof(Qnode));
+  Q->front->next = NULL;
+  return OK;
+}
+
+Status isEmpty(Queue Q){
+  if(Q.front == Q.rear)return True;
+  else return False;
+}
+
+Status enQueue(Queue *Q,ElemType e){
+  Qlink new = (Qlink)malloc(sizeof(Qnode));
+  new->data = e; 
+  new->next = NULL;
+  Q->rear->next = new;
+  Q->rear = new;
+  return OK;
+}
+
+Status deQueue(Queue *Q,ElemType *e){
+  if(isEmpty(*Q))return ERROR;
+  Qlink tmp = Q->front->next;
+  Q->front->next = tmp->next;
+  *e = tmp->data;
+  if(Q->rear == tmp)
+    Q->rear = Q->front;
+  free(tmp);
+  return OK;
+}
+
+/***********************************/
+/*		BFST		   */
+/***********************************/
 int visited[MAXNUM];
 
+void BFSTraverse(VNGraph G){
+  int i;
+  //init visited
+  for(i=0;i<G.vexnum;i++)
+    visited[i]=False;
+
+  //init Queue
+  Queue Q;
+  initQueue(&Q);
+
+  //begin breath first traverse
+  for(i=0;i<G.vexnum;i++){
+    if(!visited[i]){
+      printf("[%c] ",G.vexs[i].data); //visit
+      visited[i]=True;
+      enQueue(&Q,i);
+      while(!isEmpty(Q)){
+        int w;
+        deQueue(&Q,&w);
+        EdgeNode *neighbor = G.vexs[w].firstedge;
+        while(neighbor!=NULL){
+          if(!visited[neighbor->adjvex]){
+            printf("[%c] ",G.vexs[neighbor->adjvex].data); //visit
+            visited[neighbor->adjvex] = True;
+            enQueue(&Q,neighbor->adjvex);
+          }
+          neighbor = neighbor->nextacr;
+        }
+      }//while
+    }//if
+  }//for
+}
+
+
+/***********************************/
+/*		Create		   */
+/***********************************/
 VNGraph createDG(){
   VNGraph G;
   printf("G.vexnum: ");
@@ -52,31 +140,9 @@ VNGraph createDG(){
   return G;
 }
 
-void DFS(VNGraph G,int v);
-
-void DFSTraverse(VNGraph G){
-  int i;
-  for(i=0;i<G.vexnum;i++)
-    visited[i]=False;
-
-  for(i=0;i<G.vexnum;i++)
-    if(!visited[i])
-      DFS(G,i);
-}
-
-void DFS(VNGraph G,int v){
-  visited[v] = True;
-  EdgeNode * w = G.vexs[v].firstedge;
-  while(w!=NULL){
-    if(!visited[w->adjvex])
-      DFS(G,w->adjvex);
-    w=w->nextacr; 
-  }
-}
-
 int main(){
   VNGraph G = createDG();
-  DFSTraverse(G);
+  BFSTraverse(G);
   int i,j=0;
   for(i=0;i<G.vexnum;i++)
     if(visited[i])
