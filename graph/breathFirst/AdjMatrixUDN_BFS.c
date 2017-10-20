647c7ce4b4033300277c856d1643cb2e068bc1da
demon.zx <920248921@qq.com>
2017-10-20 19:33:11 +0800

    begin breathFirst

---
+++ graph/breathFirst/AdjMatrixUDN_BFS.c
@@ -0,0 +1,150 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define False 0
+#define True  1
+#define INFINITY 1000
+#define MAXNUM 20
+#define OK 1
+#define ERROR 0
+#define OVERFLOW -2
+
+//AdjMatrix
+typedef char VertexType;
+typedef int VRType;
+typedef struct{
+  VertexType vexs[MAXNUM];
+  VRType acrs[MAXNUM][MAXNUM];
+  int acrnum,vexnum;
+}MGraph;
+
+/*========================================*/
+/*		    Queue                 */
+/*========================================*/
+typedef int Status;
+typedef int ElemType;
+typedef struct Qnode{
+  ElemType data;
+  struct Qnode *next;
+}Qnode,*Qlink;
+
+typedef struct{
+  Qlink front;
+  Qlink rear;
+}Queue;
+
+Status initQueue(Queue *Q){
+  Q->front = Q->rear = (Qlink)malloc(sizeof(Qnode));
+  Q->front->next = NULL;
+  return OK;
+}
+
+Status isEmpty(Queue Q){
+  if(Q.front == Q.rear)return True;
+  else return False;
+}
+
+Status enQueue(Queue *Q,ElemType e){
+  Qlink new = (Qlink)malloc(sizeof(Qnode));
+  new->data = e;
+  new->next = NULL;
+  Q->rear->next = new;
+  Q->rear = new;
+  return OK;
+}
+
+Status deQueue(Queue *Q,ElemType *e){
+  if(isEmpty(*Q))return ERROR;
+  Qlink tmp = Q->front->next;
+  Q->front->next = tmp->next;
+  *e = tmp->data;
+  if(Q->rear == tmp)Q->rear = Q->front;
+  free(tmp);
+  return OK;
+}
+/*========================================*/
+/*		    BFST                  */
+/*========================================*/
+
+//global array:
+int visited[MAXNUM];
+
+void BFSTraverse(MGraph G){
+  
+  //initalizated the visited array
+  int i;
+  for(i=0;i<G.vexnum;i++)
+    visited[i]=False;
+ 
+  //create a Queue
+  Queue Q;
+  initQueue(&Q);
+ 
+  //Traverse each connected components 
+  int w,j;
+  for(i=0;i<G.vexnum;i++){
+    if(!visited[i]){
+      visited[i] = True;
+      enQueue(&Q,i);
+      while(!isEmpty(Q)){
+        deQueue(&Q,&w);
+        for(j=0;j<G.vexnum;j++){
+           if(G.acrs[w][j]==1 && !visited[j]){
+             visited[j]=True;
+             enQueue(&Q,j);
+           }
+        }
+      }
+    }//if
+  }//for
+}
+
+
+/*========================================*/
+/*		   Create                 */
+/*========================================*/
+MGraph createUDN(){
+  MGraph G;
+  printf("G.vexnum:");
+  scanf("%d",&G.vexnum);
+  printf("G.acrnum:");
+  scanf("%d",&G.acrnum);
+  getchar();
+
+  //input the vertexs
+  int i;
+  for(i=0;i<G.vexnum;i++){
+    printf("G.vexs[%d]:",i);
+    scanf("%c",&G.vexs[i]);
+    getchar();
+  }
+
+  //initialized the matrix
+  int j;
+  for(i=0;i<G.vexnum;i++)
+    for(j=0;j<G.vexnum;j++)
+      G.acrs[i][j]=INFINITY;
+
+  //input the matrix
+  int k,a,b,c;
+  for(k=0;k<G.acrnum;k++){
+    printf("[%d]i,j,v: ",k);
+    scanf("%d,%d,%d",&a,&b,&c);
+    G.acrs[a][b]=c;
+    G.acrs[b][a]=c;
+  }
+
+  printf("\n\n");
+  return G;
+}
+
+int main(){
+  MGraph G = createUDN();
+  BFSTraverse(G);
+  int i,sum=0;
+  for(i=0;i<G.vexnum;i++)
+    if(visited[i])
+      sum++;
+
+  printf("the number of the true:%d\n",sum);
+  return 0;
+}
---
+++ graph/breathFirst/OrthoDG_BFS.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAXNUM 100
+#define True 1
+#define False 0
+
+typedef int VertexType;
+typedef struct AcrBox{
+  int headvex,tailvex;
+  struct AcrBox *headlink,*taillink;
+}AcrBox;
+
+typedef struct{
+  VertexType data;
+  struct AcrBox *firstin,*firstout;
+}VexNode;
+
+typedef struct{
+  VexNode xlist[MAXNUM];
+  int acrnum,vexnum;
+}OLGraph;
+
+OLGraph createDG(){
+  OLGraph G;
+  printf("G.vexnum: ");
+  scanf("%d",&G.vexnum);
+  printf("G.acrnum: ");
+  scanf("%d",&G.acrnum);
+  getchar();
+
+  int i;
+  for(i=0;i<G.vexnum;i++){
+    printf("G.xlist[%d].data:",i);
+    scanf("%d",&G.xlist[i].data);
+    getchar();
+  }
+
+  int a,b;
+  for(i=0;i<G.acrnum;i++){
+    printf("[%d]acr->i,j: ",i);
+    scanf("%d,%d",&a,&b);
+    getchar();
+    AcrBox *new   = (AcrBox*)malloc(sizeof(AcrBox));
+    new->headvex  = b;
+    new->tailvex  = a;
+    new->headlink = G.xlist[b].firstin;
+    new->taillink = G.xlist[a].firstout;
+    G.xlist[b].firstin  = new;
+    G.xlist[a].firstout = new;
+  }
+  return G;
+}
+
+//Depth First Traverse 
+int visited[MAXNUM];
+
+void DFS(OLGraph G,int v);
+
+void DFSTraverse(OLGraph G){
+  int i;
+  for(i=0;i<G.vexnum;i++)
+    visited[i] = False;
+  
+  for(i=0;i<G.vexnum;i++)
+    if(!visited[i])
+      DFS(G,i);
+}
+
+void DFS(OLGraph G,int v){ 
+  visited[v] = True;
+  AcrBox *w = G.xlist[v].firstout;
+  while(w!=NULL){
+    if(!visited[w->headvex])
+      DFS(G,w->headvex);
+    w=w->headlink;
+  }
+}
+
+int main(){
+  OLGraph G = createDG();
+  DFSTraverse(G);
+  int i,j=0;
+  for(i=0;i<G.vexnum;i++)
+    if(visited[i])
+      j++;
+
+  printf("the number of true is %d\n",j);
+  return 0;
+}
