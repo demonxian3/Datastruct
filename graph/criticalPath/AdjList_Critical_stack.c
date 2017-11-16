adb4a7b386e8c37106b5794d6664fb9ac33dc5b3
demon.zx <920248921@qq.com>
2017-11-16 15:17:44 +0800

    fixing

--- graph/criticalPath/AdjList_Critical_stack.c
+++ graph/criticalPath/AdjList_Critical_stack.c
@@ -73,37 +73,66 @@ void printDN(ALGraph G){
  *    Critical Paht    *
  ***********************/
 int ve[MAXNUM];
+
 void findIndegree(ALGraph G,int *indegree){
   int i;
-  EdgeNode *p = (EdgeNode*)malloc(sizeof(EdgeNode));  
   for(i=0;i<G.vexnum;i++)
     indegree[i]=0;
 
   for(i=0;i<G.vexnum;i++){
-    p = G.vexs[i].firstedge;
-    indegree[p->adjvex]++;
-    p = p->nextacr;
+    EdgeNode *p = G.vexs[i].firstedge;
+    while(p){
+      indegree[p->adjvex]++;
+      p=p->nextacr;
+    }
   }
- 
+
+
 }
 
 Status TopoLogicalSort(ALGraph G,Sqstack *T){
-  int i;
-  int *indegree = (int*)malloc(G.vexnum*sizeof(int));
-  EdgeNode *tmp = (EdgeNode*)malloc(sizeof(EdgeNode));
+  int i,j;
+  int count = 0;
+  int indegree[G.vexnum];
   Sqstack S;
   initStack(&S);
   findIndegree(G,indegree);
-  initStack(T);
 
   for(i=0;i<G.vexnum;i++)
-    printf("%d\n",G.vexs[i].data);
+    if(!indegree[i]) 
+      pushStack(&S,i);
+
+  for(i=0;i<G.vexnum;i++)
+    ve[i]=0;
+
+  while(!isEmpty(S)){
+    count++;
+    popStack(&S,&j);
+    pushStack(&S,j);
+    
+    //求ve 和 删点j
+    EdgeNode *p = (EdgeNode*)malloc(sizeof(EdgeNode));
+    while(p){
+      if( ve[j]+p->info > ve[p->adjvex] )
+        ve[p->adjvex] = ve[j] + p->info;
+      if(--indegree[p->adjvex] == 0)
+        pushStack(&S,j);
+      p = p->nextacr;
+    }
+  }
+
+  if(count<G.vexnum)return ERROR; //0
+  else return OK; //1
 }
 
 Status CriticalPath(ALGraph G){
   Sqstack T;
-  if(!TopoLogicalSort(G,&T))
+  initStack(&T);
+  if(!TopoLogicalSort(G,&T)){
+    printf("The AOE have loop!!\n");
     return ERROR;
+  }
+ 
   
 }
 
