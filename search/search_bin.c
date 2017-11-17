b655066a70cf4c69512785d022e26bd6522cc5f5
demonxian3 <demonxian3@qq.com>
2017-11-17 20:55:45 +0800

    add counter for search_bin and compare count for same input

--- search/search_bin.c
+++ search/search_bin.c
@@ -19,6 +19,7 @@ void createTestData(ElemType *data,int n){
 }
 
 int search_bin(ElemType *data,KeyType key,int n){
+  int count = 0; //statistics
   int low,high,mid;
 
   low = 1;
@@ -26,15 +27,25 @@ int search_bin(ElemType *data,KeyType key,int n){
   mid = (low+high)/2;
   
   while(data[mid].key!=key){
+    count++;
     if(key<data[mid].key)
       high = mid - 1;
     else
       low = mid + 1;
 
     mid = (low+high)/2;
-    if(low>=mid || high<=low)return 0;
+    if(high<low){          /*
+                            * can't use the "low<=mid||high<=low",
+                            * because it exclude the boundary element;
+                            */
+                                          
+      printf("[count]: %d  ",count);
+      return 0;
+    }
   }
   
+  if(count==0)count++;
+  printf("[count]: %d  ",count);
   return mid;
 }
 
@@ -60,3 +71,51 @@ int main(){
       printf("Found it,the key's index is:%d\n",res);
   }
 }
+
+/* [Test Data]
+Enter the number of key: 8
+[1]Enter the value: 7
+[2]Enter the value: 14
+[3]Enter the value: 17
+[4]Enter the value: 22
+[5]Enter the value: 28
+[6]Enter the value: 31
+[7]Enter the value: 34
+[8]Enter the value: 44
+[1]: 7 
+[2]: 14 
+[3]: 17 
+[4]: 22 
+[5]: 28 
+[6]: 31 
+[7]: 34 
+[8]: 44 
+Enter the key value: 5
+[count]: 3  Sorry, the key was not found
+Enter the key value: 7
+[count]: 2  Found it,the key's index is:1
+Enter the key value: 13
+[count]: 3  Sorry, the key was not found
+Enter the key value: 14
+[count]: 1  Found it,the key's index is:2
+Enter the key value: 17
+[count]: 2  Found it,the key's index is:3
+Enter the key value: 22
+[count]: 1  Found it,the key's index is:4
+Enter the key value: 24
+[count]: 3  Sorry, the key was not found
+Enter the key value: 28
+[count]: 2  Found it,the key's index is:5
+Enter the key value: 31
+[count]: 1  Found it,the key's index is:6
+Enter the key value: 34
+[count]: 2  Found it,the key's index is:7
+Enter the key value: 44
+[count]: 3  Found it,the key's index is:8
+Enter the key value: 49
+[count]: 4  Sorry, the key was not found
+
+  25/12 = 2
+
+*/
+
--- search/search_bin2.c
+++ search/search_bin2.c
@@ -16,18 +16,107 @@ typedef struct {
   int length;
 }Stable;
 
-void createTestData(Stable *ST,int n){
-  int i;
-  for(i=1;i<=n;i++){
 
+void createTestData(Stable *ST,int n){
+  int i,val;
+  ST->elem = (ElemType *)malloc(n *sizeof(ElemType));
+  ST->length = n;
+  for(i=1;i<=n;i++){
+    printf("[%d]input value: ",i);
+    scanf("%d",&val);
+    ST->elem[i].key = val;
   }
+
+  //output and debug
+  for(i=1;i<=n;i++)
+    printf("[%d] %d\n",i,ST->elem[i].key);
+}
+
+int search_bin(Stable ST,KeyType key){
+  
+  int count = 0; //statistics
+  int low = 1;
+  int len = ST.length;
+  int high = len;
+  
+  while(low<=high){               //include '=',because match boundary element.
+    count++;
+    int mid = (low+high)/2;
+    if(key == ST.elem[mid].key){   //found it
+      printf("[count]:%3d  ",count);
+      return mid;
+    }
+    else if(key < ST.elem[mid].key)  //before area
+      high = mid-1;
+    else
+      low = mid+1;
+  }
+  
+  printf("[count]:%3d  ",count);
+  return 0;
 }
 
 int main(){
-  int n;
-  Stable ST
+  int n,r,k;
+  Stable ST;
+
   printf("Enter the number of key: ");
   scanf("%d",&n);
-  Stable ST = createTestData(n);
+  createTestData(&ST,n);
+
+  while(1){
+    printf("Enter the key value for search: ");
+    scanf("%d",&k);
+
+    if(!(r = search_bin(ST,k)))
+      printf("Sorry,the key is not found\n");
+    else
+      printf("Found it,the key's index is:%d\n",r);
+  }
   return 0;
 }
+
+/*
+Enter the number of key: 8
+[1]input value: 7  
+[2]input value: 14
+[3]input value: 17
+[4]input value: 22
+[5]input value: 28
+[6]input value: 31
+[7]input value: 34
+[8]input value: 44
+[1] 7
+[2] 14
+[3] 17
+[4] 22
+[5] 28
+[6] 31
+[7] 34
+[8] 44
+Enter the key value for search: 5
+[count]:  3  Sorry,the key is not found
+Enter the key value for search: 7
+[count]:  3  Found it,the key's index is:1
+Enter the key value for search: 13
+[count]:  3  Sorry,the key is not found
+Enter the key value for search: 14
+[count]:  2  Found it,the key's index is:2
+Enter the key value for search: 17
+[count]:  3  Found it,the key's index is:3
+Enter the key value for search: 22
+[count]:  1  Found it,the key's index is:4
+Enter the key value for search: 24
+[count]:  3  Sorry,the key is not found
+Enter the key value for search: 28
+[count]:  3  Found it,the key's index is:5
+Enter the key value for search: 31
+[count]:  2  Found it,the key's index is:6
+Enter the key value for search: 34
+[count]:  3  Found it,the key's index is:7
+Enter the key value for search: 44
+[count]:  4  Found it,the key's index is:8
+Enter the key value for search: 49
+[count]:  4  Sorry,the key is not found
+  34 / 12 = 2.8
+*/
