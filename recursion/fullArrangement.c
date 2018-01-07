391d87aaeabd22119bcb75674f1104cb3c71b918
demon.zx <920248921@qq.com>
2018-01-07 20:14:56 +0800

    add **FullArrangeMent**

---
+++ recursion/fullArrangement.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+int n=0;
+
+void swap(int *a, int *b)
+{
+     int m;
+     m=*a;
+     *a=*b;
+     *b=m;
+}
+void perm(int list[], int k, int m)
+{
+     int i;
+     if(k==m)
+     {
+             for(i=0;i<=m;i++)
+                              printf("%d ",list[i]);
+             printf("\n");
+             n++;
+     }
+     else
+     {
+         for(i=k;i<=m;i++)
+         {
+                          swap(&list[k],&list[i]);
+                          perm(list, k+1, m);
+                          swap(&list[k], &list[i]);
+         }
+     }
+}       
+int main(void)
+{
+    int list[]={1,2,3,4,5,6,7,8};
+    perm(list,0,7);
+    printf("total:%d\n",n);
+    return 0;
+}
