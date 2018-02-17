8f8c1de46005ec4da2eea9f758d02b9220abd5ab
demonxian3 <demonxian3@qq.com>
2018-02-18 00:11:49 +0800

    move

--- stack/sgStack_str.h
+++ stack/sgStack_str.h
@@ -1,8 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
 #define STACKLEN 100
 #define TRUE 1
 #define FALSE 0
@@ -82,54 +77,3 @@ void printStack(Stack S){
     }
     printf("\n");
 }
-
-int main(){
-    int c;
-    Stack S;
-    char push[100];
-    initStack(&S);
-    printStack(S);
-    
-    //push
-    printf("Enter push string: ");
-    scanf("%s",push);
-    pushStack(S,push);
-    printStack(S);
-
-    //push
-    printf("Enter push string: ");
-    scanf("%s",push);
-    pushStack(S,push);
-    printStack(S);
-    //push
-    printf("Enter push string: ");
-    scanf("%s",push);
-    pushStack(S,push);
-    printStack(S);
-    //push
-    printf("Enter push string: ");
-    scanf("%s",push);
-    pushStack(S,push);
-    printStack(S);
-    //push
-    printf("Enter push string: ");
-    scanf("%s",push);
-    pushStack(S,push);
-    printStack(S);
-
-    //pop
-    char *str = (char*)malloc(sizeof(char)*100);
-    popStack(S,&str);
-    printf("pop Value: %s\n",str);
-    printStack(S);
-
-    //pop
-    popStack(S,&str);
-    printf("pop Value: %s\n",str);
-    printStack(S);
-    //pop
-    popStack(S,&str);
-    printf("pop Value: %s\n",str);
-    printStack(S);
-}
-
